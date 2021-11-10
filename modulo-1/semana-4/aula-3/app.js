49b1bfcbd079b96aa804cca4cc314d1dc1104fec
romeuic <casarottori@gmail.com>
2021-11-10 01:10:00 -0300

    adiciona base dos exemplos do projeto-1

---
+++ modulo-1/semana-4/aula-3/app.js
@@ -0,0 +1,95 @@
+// pega elementos do html
+const campo = document.querySelector('input[name=campo]')
+const botaoAdd = document.getElementById('btn-adicionar')
+const botaoClear = document.getElementById('btn-limpar')
+const ulAtividades = document.getElementById('ul-atividades')
+
+// inicia um vetor vazio
+let lista = []
+
+let listaStorage = localStorage.getItem('lista-atividades')
+
+if (listaStorage) {
+  console.log(listaStorage)
+}
+
+// apaga conteudo do campo
+function limpaCampo() {
+  campo.value = ''
+}
+
+function removeItem(index) {
+  const novaLista = []
+
+  for (let i = 0; i < lista.length; i++) {
+    if (i !== index) {
+      novaLista.push(lista[i])
+    }
+  }
+
+  const itemParaRemocao = document.getElementById(`item-${index}`)
+
+  ulAtividades.removeChild(itemParaRemocao)
+
+  lista = novaLista
+}
+
+function marcaFeito(index) {
+  if (lista[index]) {
+    const item = lista[index]
+
+    item.feito = !(item.feito)
+
+    const elemento = document.getElementById(`item-${index}`)
+
+    elemento.className = item.feito ? 'feito' : ''
+  }
+}
+
+// adiciona itens a lista
+function adiciona() {
+
+  if (campo.value.length > 0) {
+
+    const item = {
+      id: lista.length,
+      titulo: campo.value,
+      feito: false
+    }
+
+    lista.push(item)
+
+    const novoElemento = document.createElement('li')
+
+    novoElemento.id = `item-${item.id}`
+
+    novoElemento.innerHTML = `
+      <input
+        type="checkbox"
+        name="chk-${item.id}"
+        onclick="marcaFeito(${item.id})"
+      >
+      <label for="chk-${item.id}">
+        ${item.titulo}
+      </label>
+      <button onclick="removeItem(${item.id})">
+        &times;
+      </button>
+    `
+
+    ulAtividades.appendChild(novoElemento)
+
+    console.log("LISTA->", { lista })
+
+    const listaJSON = JSON.stringify(lista)
+
+    localStorage.setItem('lista-atividades', listaJSON)
+
+    limpaCampo()
+  } else {
+    alert('É preciso informar um valor!')
+  }
+}
+
+// vincula funções aos elementos html
+botaoAdd.addEventListener('click', adiciona)
