697781976d11609a8e6fe66b9a81902cd15c031a
romeuic <casarottori@gmail.com>
2021-11-14 22:32:51 -0300

    adiciona arquivos base da m1-s5-a1

---
+++ modulo-1/semana-5/aula-1/js/app.js
@@ -0,0 +1,5 @@
+import Task from './task.js'
+
+const task = new Task('Foo', 'Bar', false)
+
+console.log('HI', task)
---
+++ modulo-1/semana-5/aula-1/js/task.js
@@ -0,0 +1,7 @@
+export default class Task {
+  constructor(title, description, state) {
+    this.title = title
+    this.description = description
+    this.state = state
+  }
+}
