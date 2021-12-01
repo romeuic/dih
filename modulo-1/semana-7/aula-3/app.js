5a48fb6694375144cadeb2b6941058c31c01e5a0
romeuic <casarottori@gmail.com>
2021-12-01 01:02:33 -0300

    atualiza exemplo m1-s7-a3

--- modulo-1/semana-7/aula-3/app.js
+++ modulo-1/semana-7/aula-3/app.js
@@ -44,6 +44,7 @@ const buscaCEPfetch = async () => {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
     const data = await response.json()
     console.log(data)
+    pTela.innerHTML = data.logradouro
   } catch (err) {
     console.error(err)
   }
