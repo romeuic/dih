4e4b2bae8f58372f1926c08bd832cce6b98fd114
romeuic <casarottori@gmail.com>
2021-12-01 21:28:00 -0300

    atualiza exemplo m1-s7-a3

--- modulo-1/semana-7/aula-3/app.js
+++ modulo-1/semana-7/aula-3/app.js
@@ -3,34 +3,74 @@ const botaoCep = document.querySelector('#botao-cep')
 const pTela = document.querySelector('#p-tela')
 
 
-const delay = ms => new Promise(res => setTimeout(res, ms))
+// Promises
 
+// const testaSeEh2 = numero => {
+//   return new Promise((resolve, reject) => {
+//     if (numero === 2) {
+//       resolve('Eh 2!')
+//     } else {
+//       reject('Batata!')
+//     }
+//   })
+// }
 
-const buscaCEP = async () => {
+//testaSeEh2(10)
+//   .then(r => console.log(r))
+//   .catch(e => console.error('CATCH: ' + e))
+//   .finally(() => console.log('Sempre executo!'))
 
-  try {
+// fetch(`https://viacep.com.br/ws/9728000/json`)
+//   .then(response => response.json())
+//   .then(objeto => console.log(objeto))
+//   .catch(erro => console.log('CATCH: ' + erro))
 
-    await delay(1000)
-    console.log('Terminei de esperar')
+// async function buscaCepEx() {
+//   try {
+//     const cep = campoCep.value
+//     const URL = `https://viacep.com.br/ws/${cep}/json`
+//     const response = await fetch(URL)
+//     const { localidade, logradouro } = await response.json()
 
-  } catch (err) {
-    console.error(err)
-  }
+//     pTela.innerHTML = `
+//       Localidade: ${localidade}<br>
+//       Logradouro: ${logradouro}
+//     `
+//   } catch (erro) {
+//     console.error('CATCH: ' + erro)
+//   }
+// }
+
+// botaoCep.addEventListener('click', buscaCepEx)
+
+function buscaCEPpromise() {
+  const cep = campoCep.value
+  const URL = `https://viacep.com.br/ws/${cep}/json`
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      pTela.innerHTML = data.logradouro
+    })
+    .catch(err => {
+      console.error(err)
+    })
 }
 
-
-buscaCEP()
-console.log('Executei o console log')
-
-
-const buscaCEPfetch = async () => {
+// cria a função ass[incrna]
+async function buscaCEPfetch() {
   try {
     const cep = campoCep.value
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+    const URL = `https://viacep.com.br/ws/${cep}/json`
+    // espera a promise de fetch ser resolvida
+    const response = await fetch(URL)
+    // espera a promise response.json ser resolvida
     const data = await response.json()
+    // tem todos dados necessários para o resto da lógica
     console.log(data)
     pTela.innerHTML = data.logradouro
   } catch (err) {
+    // se der algum problema em try, executa o catch
     console.error(err)
   }
 }
