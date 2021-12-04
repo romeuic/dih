35dbce29d01a00b1a2fa36f756e248ab57d58dba
romeuic <casarottori@gmail.com>
2021-12-03 21:48:35 -0300

    atualiza m1-s7-td

--- modulo-1/semana-7/tira-duvidas/app.js
+++ modulo-1/semana-7/tira-duvidas/app.js
@@ -59,3 +59,13 @@ const objB = { d: 4, e: 5, f: 6 }
 const objC = { ...objB, ...objA }
 
 console.log(objC)
+
+// Exempo: Semana5 - Ex.3
+class Endereco {
+  constructor(l, n, c) {
+    const params = [l, n, c]
+    if (params.some(p => typeof p !== 'string')) {
+      throw 'Endereço inválido!'
+    }
+  }
+}
